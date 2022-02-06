7efb2dac3960f87c9aaca82e9b63df101241d0dd
Luis Amorim <luisfelipeamorim@hotmail.com>
2022-02-05 21:50:40 -0300

    Fix: Added funnel for themes and views alias

---
+++ src/themes/index.ts
@@ -0,0 +1,11 @@
+import furret from "./furret.theme";
+import oddish from "./oddish.theme";
+import mimikyu from "./mimikyu.theme";
+import initial from "./initial.theme";
+
+export {
+    furret,
+    oddish,
+    mimikyu,
+    initial
+}
---
+++ src/views/index.ts
@@ -0,0 +1,9 @@
+import Home from "./home/home.view";
+import Details from "./details/details.view";
+import Save from "./save/save.view";
+
+export {
+    Home,
+    Details,
+    Save
+}
