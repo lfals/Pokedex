d9baf5dd21a735fbd49ce887c5b8697044087bdd
Luis Amorim <luisfelipeamorim@hotmail.com>
2022-02-05 21:49:27 -0300

    Setup: Added Furret Themes

---
+++ src/themes/furret.theme.ts
@@ -0,0 +1,12 @@
+import image from "../assets/furret.png"
+
+
+const furret = {
+    primary: "#9B7363",
+    secondary: "#F2D7B8",
+    tertiary: "#434343",
+    background: image
+
+}
+
+export default furret
